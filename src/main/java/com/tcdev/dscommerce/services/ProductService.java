d3c276ba0b5af7acd3bfa79c57c24ad1e4f31ab7
tiago-costa-s <tiagocostasantos0013@gmail.com>
2026-06-06 08:49:21 -0300

    GET products paged

--- src/main/java/com/tcdev/dscommerce/services/ProductService.java
+++ src/main/java/com/tcdev/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.tcdev.dscommerce.dto.ProductDTO;
 import com.tcdev.dscommerce.entities.Product;
 import com.tcdev.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +22,11 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> results = repository.findAll(pageable);
+        return results.map(x -> new ProductDTO(x));
     }
 }
